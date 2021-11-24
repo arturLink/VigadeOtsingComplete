a68b25d2d881a357568cf0855b6d91f15416b3bc
opilane <opilane@HP-CZC6297B8Z>
2021-11-24 10:08:35 +0200

    finished

--- VigadeOtsing.py
+++ VigadeOtsing.py
@@ -5,26 +5,24 @@ while 1:
         break
     except ValueError:
          print("Это не целое число")
-#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-if a==0:
+#vodim 4islo eshe proverka
+if a==0:#esli 0 to vivod na ekran
     print("Нет смысла ничего делать с нулём")
 else:
-#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     print("Определяем, сколько в числе чётных и сколько нечётных цифр")
-    print()
-    c=b=a
-    paaris=0
+    c=b=a#prirovnjat peremen
+    paaris=0#s4etchiki
     paaritu=0
     while b > 0:
-        if b % 2==0:
+        if b % 2==0:#proverka 4et li 4islo
             paaris+=1
-        else:
+        else:#v drugih slu4ajah ne 4et tak 4to + v s4etchik
             paaritu+=1
         b = b // 10
     
     print("Чётных цифр:",paaris)
     print("Нечётных цифр:",paaritu)
-#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#skok 4et i ne 4et
     print("Переворачиваем введённое число")
     b=0
     while a > 0:
@@ -33,16 +31,17 @@ else:
         b=b * 10
         b+=number
     print("Перевёрнутое число", b)
-#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#perevorot 4isla
     print("Проверяем гипотезу Сиракуз")
-    if c%2==0:
+    if c%2==0:#esli 4et to vivod 4isla
         print(c," - чётное число. Делим на 2.")
-    else:
+    else:#v drugih slu4ajah ne 4et 
         print(c," - нечётное число. Умножаем на 3, прибавляем 1 и делим на 2.")
-    while c!= 1:
-        if c%2==0:
-            c=c / 2
-        else:
+    while c!= 1:#tolko esli c=1 vipolnajatsa vse 4to nishe
+        if c%2==0:#4et ili ne 4et
+            c=c / 2#delit esli 4et
+        else:#esli ne 4et sdelat 4et
             c = (3*c + 1) / 2
     print(c, end=" ")
     print("Гипотеза верна")
+#proverjaem gipotezu
